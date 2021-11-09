254aeef0fb2fb585f20c1e8ecd2d884eee99a010
job karani <job.nyambura@student.moringaschool.com>
2021-11-09 11:32:24 +0300

    creates a function to add a category'

---
+++ app/main/views.py
@@ -0,0 +1,26 @@
+from flask import render_template, redirect, url_for, abort, flash, request
+from . import main
+from flask_login import login_required, current_user
+
+from app.models import User, Post, Comment, Category
+# from .. import db,images
+from .forms import CategoryForm
+
+
+
+@main.route('/')
+def index():
+    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    print(posts)
+    return render_template('index.html', posts=posts)
+
+@main.route('/add_category', methods=['GET', 'POST'])
+def add_cat():
+    form = CategoryForm()
+    if form.validate_on_submit():
+        category = Category(name=form.name.data)
+        db.session.add(category)
+        db.session.commit()
+        flash('Category added successfully.')
+        return redirect(url_for('.index'))
+    return render_template('add_category.html', form=form)

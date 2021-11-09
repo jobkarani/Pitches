0ca0e3779c073fdffe4981aba7bbcda9ec2a23c3
job karani <job.nyambura@student.moringaschool.com>
2021-11-09 10:33:05 +0300

    creates class comment'

--- app/models.py
+++ app/models.py
@@ -59,3 +59,18 @@ class Post(db.Model):  # post table
     def __repr__(self):
         return f'Post {self.title}'
 
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Comment {self.content}'
+

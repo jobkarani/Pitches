e6fce0fb8f139feff78e46603e36530af5fed3b2
job karani <job.nyambura@student.moringaschool.com>
2021-11-09 10:55:41 +0300

    adds a password column

--- app/models.py
+++ app/models.py
@@ -33,6 +33,19 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.hashed_pass = generate_hashed_pass(password)
+
+    def verify_password(self, password):
+        return check_hashed_pass(self.password_hash, password)
+
+    
+
     def save_user(self):
         db.session.add(self)
         db.session.commit()

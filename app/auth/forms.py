59ed99459d30cc59d2341c51807d2ca2fea783ad
job karani <job.nyambura@student.moringaschool.com>
2021-11-09 13:02:42 +0300

    creates registration form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -11,3 +11,19 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign In')
 
 
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(),
+    EqualTo('password2',message = 'Passwords must match')])
+    password2 = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
+
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email =data_field.data).first():
+            raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
